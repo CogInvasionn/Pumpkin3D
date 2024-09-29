7716c28a22773e80d4eaac9de04ae156bc9a062e
Chlove <v4nished@protonmail.com>
2024-09-29 14:15:24 -0400

    Create help.py

---
+++ cli tool/help.py
@@ -0,0 +1,7 @@
+def help_command():
+    print("""
+    Available Commands:
+    - misc welcome : Display the welcome message
+    - misc version : Show the version of the script
+    - misc help    : Show this help message
+    """)
